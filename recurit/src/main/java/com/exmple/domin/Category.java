edb0a938203267a843e35b4e57f215144f0a8795
wxx <2313084083@qq.com>
2022-02-18 23:10:48 +0800

    feat:index页面改造

--- recurit/src/main/java/com/exmple/domin/Category.java
+++ recurit/src/main/java/com/exmple/domin/Category.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class Category {
-    private String cid;
+    private Integer cid;
     private String cname;
 }
--- recurit/src/main/java/com/exmple/service/impl/CategoryServiceImpl.java
+++ recurit/src/main/java/com/exmple/service/impl/CategoryServiceImpl.java
@@ -4,8 +4,12 @@ import com.exmple.dao.CategoryDao;
 import com.exmple.dao.impl.CategoryDaoImpl;
 import com.exmple.domin.Category;
 import com.exmple.service.CategoryService;
+import com.exmple.utils.JedisUtil;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CategoryServiceImpl implements CategoryService {
 
@@ -17,6 +21,43 @@ public class CategoryServiceImpl implements CategoryService {
      */
     @Override
     public List<Category> findAll() {
-        return categoryDao.findAll();
+        //获取jedis客户端
+        Jedis jedis = JedisUtil.getJedis();
+        //使用sortedset排序查询
+        Set<String> category= jedis.zrange("category",0,-1);
+        List<Category>list=null;
+        //判断集合是否存在category
+        if (category==null||category.size()==0){//集合为空从数据库中查询
+            list=categoryDao.findAll();
+            //储存在redis中
+            for (Category c :
+                    list) {
+                jedis.zadd("category",c.getCid(),c.getCname());
+                System.out.println(c.getCid());
+            }
+            //重新从缓存中读取
+            category= jedis.zrange("category",0,-1);
+            list=getInfoFromRedis(category);
+        }else {//集合不为空封装成list返回
+            list=getInfoFromRedis(category);
+        }
+        return list;
+    }
+
+    /**
+     * 将缓存中的数据set封装成list
+     * @param category
+     * @return
+     */
+    private List<Category> getInfoFromRedis(Set<String> category){
+        List<Category> list=new ArrayList<Category>();
+        for (String s :
+                category) {
+            Category c = new Category();
+            c.setCname(s);
+            System.out.println(s);
+            list.add(c);
+        }
+        return list;
     }
 }
